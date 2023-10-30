1408cc5a59c5499b335057997f632ee579eed67d
QuiD-0 <wodnd101@naver.com>
2023-10-30 17:59:20 +0900

    불필요한 코드 정리

--- src/main/kotlin/com/quid/auth/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/quid/auth/global/security/SecurityConfig.kt
@@ -1,14 +1,10 @@
 package com.quid.auth.global.security
 
-import com.quid.auth.user.usecase.UserAuthService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import org.springframework.security.web.util.matcher.RequestMatcher
@@ -28,9 +24,6 @@ class SecurityConfig {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .build()
-
-    @Bean
-    fun byCryptPasswordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     private fun allow(): Array<RequestMatcher> = arrayOf(
         AntPathRequestMatcher("/", "GET"),
