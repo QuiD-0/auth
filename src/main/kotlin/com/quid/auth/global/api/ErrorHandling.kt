fa74c11aa615af4dab79eb7aa0dfb57747f4558d
QuiD-0 <wodnd101@naver.com>
2023-11-03 10:16:49 +0900

    HttpStatus결정 객체 분리

--- src/main/kotlin/com/quid/auth/global/api/ErrorHandling.kt
+++ src/main/kotlin/com/quid/auth/global/api/ErrorHandling.kt
@@ -1,7 +1,5 @@
 package com.quid.auth.global.api
 
-import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -13,16 +11,8 @@ class ErrorHandling {
     fun handleException(ex: Exception): ResponseEntity<Error> =
         ResponseEntity<Error>(
             Error{ ex.message ?: "Unknown Error" },
-            determineStatusCode(ex)
+            HttpStatusSelector { ex }
         ).also { ex.printStackTrace() }
 
-    private fun determineStatusCode(ex: Exception): HttpStatus =
-        when (ex) {
-            is IllegalArgumentException -> BAD_REQUEST
-            is NoSuchElementException -> NOT_FOUND
-            is IllegalStateException -> CONFLICT
-            is RuntimeException -> INTERNAL_SERVER_ERROR
-            else -> INTERNAL_SERVER_ERROR
-        }
 
 }
