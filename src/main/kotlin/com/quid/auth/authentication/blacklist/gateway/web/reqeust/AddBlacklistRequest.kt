e6d61fd7e57bb93b1a1d138556baf178c31e8d02
QuiD-0 <wodnd101@naver.com>
2023-11-14 16:16:09 +0900

    user exist check

--- src/main/kotlin/com/quid/auth/authentication/blacklist/gateway/web/reqeust/AddBlacklistRequest.kt
+++ src/main/kotlin/com/quid/auth/authentication/blacklist/gateway/web/reqeust/AddBlacklistRequest.kt
@@ -6,12 +6,17 @@ import java.time.LocalDateTime
 data class AddBlacklistRequest(
     val userSeq: Long,
     val reason: String,
-    val expiredDate: LocalDateTime?
+    val expiredDays: Long
 ) {
     fun toBlacklist(): Blacklist =
         Blacklist(
             userSeq = userSeq,
             description = reason,
-            expireDate = expiredDate ?: LocalDateTime.parse("9999-12-31T23:59:59")
+            expireDate = getExpiredDate(expiredDays)
         )
+
+    private fun getExpiredDate(expiredDays: Long): LocalDateTime {
+        if (expiredDays == 0L) return LocalDateTime.parse("9999-12-31T23:59:59")
+        return LocalDateTime.now().plusDays(expiredDays)
+    }
 }
--- src/main/kotlin/com/quid/auth/authentication/blacklist/usecase/BlackUser.kt
+++ src/main/kotlin/com/quid/auth/authentication/blacklist/usecase/BlackUser.kt
@@ -2,17 +2,20 @@ package com.quid.auth.authentication.blacklist.usecase
 
 import com.quid.auth.authentication.blacklist.domain.Blacklist
 import com.quid.auth.authentication.blacklist.gateway.repository.BlacklistRepository
+import com.quid.auth.authentication.user.gateway.repository.UserRepository
 import org.springframework.stereotype.Service
 
 interface BlackUser {
-    operator fun invoke(blacklist: Blacklist)
+    operator fun invoke(blacklist: Blacklist): Blacklist
 
     @Service
     class BlackUserUseCase(
-        private val blacklistRepository: BlacklistRepository
+        private val blacklistRepository: BlacklistRepository,
+        private val userRepository: UserRepository
     ) : BlackUser {
-        override fun invoke(blacklist: Blacklist) {
-            blacklistRepository.save(blacklist)
-        }
+        override fun invoke(blacklist: Blacklist) =
+            takeIf { userRepository.existsById(blacklist.userSeq) }
+                ?.let { blacklistRepository.save(blacklist) }
+                ?: throw IllegalArgumentException("User not found: ${blacklist.userSeq}")
     }
 }
